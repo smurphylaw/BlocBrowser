355dbca95b42cd8dd78c1d7baf5b4e2d27fa9c6c
Stephen Murphy <smurphylaw@gmail.com>
2014-08-18 23:24:46 -0600

    Functional web browser

---
+++ BlocBrowser/BLCWebBrowserViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLCWebBrowserViewController.h
+//  BlocBrowser
+//
+//  Created by Murphy on 8/18/14.
+//  Copyright (c) 2014 Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCWebBrowserViewController : UIViewController
+
+@end
